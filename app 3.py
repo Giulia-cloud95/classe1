1453397639e5c9ad22821a77b56a488fd2fd45a6
Giulia-cloud95 <giuliacolletta24@outlook.it>
2025-05-20 13:50:17 +0200

    Update app 3.py

--- app 3.py
+++ app 3.py
@@ -25,7 +25,7 @@ from langchain_community.chat_models import ChatOpenAI
 st.header("Azienda")
 
 from PIL import Image
-logo = Image.open("Robottin0.jpg")
+logo = Image.open("Robottino HR.jpg")
 st.image(logo, width=200)
 # st.image(logo, use_column_width=True)
 
